1e6191deafac6b2cd6334ddaef3bc17e42e3f847
Brenda Souza <breudes@outlook.com>
2025-08-03 21:58:59 -0300

    adjust on init.sql

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,18 +1,18 @@
 -- Inserting test users
 INSERT INTO users (name, preferred_name, email, username, password, user_role, address, last_update_date, active)
 VALUES
-  ('Admin User', 'Admin', 'admin@example.com', 'admin', '$2a$10$Xy1hVnLk5OyqTn8mQ0b1Le0Y3Zr/5DzEotdc1z93idBPq8ybj2Ynu', 'ADMIN', NULL, CURRENT_TIMESTAMP, TRUE),
-  ('Client User', 'Client', 'client@example.com', 'client', '$2a$10$3q7HgN8Z3mZrj/H7hF4u7uYqj09fNqGi5DLkKT6uQf0aQjF4asJ6O', 'CLIENT', NULL, CURRENT_TIMESTAMP, TRUE),
-  ('Owner User', 'Owner', 'owner@example.com', 'owner', '$2a$10$F8tL5EiO4Pd1J1eS3/OcTO3IrzQFqKa95eS0XYxs.4IzoqGwCHpE6', 'OWNER', NULL, CURRENT_TIMESTAMP, TRUE);
+  ('Admin User', 'Admin', 'admin@example.com', 'admin', '$2a$12$VaaJlrjOtB0m.gNMib6sQe1Eyq.ykgOoFW4SZ0heD2VaYRPd50WIa', 'ADMIN', NULL, CURRENT_TIMESTAMP, TRUE),
+  ('Client User', 'Client', 'client@example.com', 'client', '$2a$12$cfLfEGGa77e15LZPK8oJ7OHkcg8HEMix//0xVZI/gg4d7ijawaf96', 'CLIENT', NULL, CURRENT_TIMESTAMP, TRUE),
+  ('Owner User', 'Owner', 'owner@example.com', 'owner', '$2a$12$qfPIlFexdjLmnKpPizpEKurlBkSJrMsHzMLqAXRzQ5bwBIPOmqqGO', 'OWNER', NULL, CURRENT_TIMESTAMP, TRUE);
 
 -- Relating entity client to user with username 'client'
-INSERT INTO client (id_user, active, document)
+INSERT INTO clients (id_user, active, document)
 SELECT id, TRUE, '12345678900'
 FROM users
 WHERE username = 'client';
 
 -- Relating entity owner to user with username 'owner'
-INSERT INTO owner (id_user, active, document)
+INSERT INTO owners (id_user, active, document)
 SELECT id, TRUE, '98765432100'
 FROM users
 WHERE username = 'owner';
